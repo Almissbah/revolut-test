f59d50d7d23b11f8f8e3934e883fa70b71d5eedf
mohammed almissbah <mohammed.almissbah@hotmail.com>
2019-12-27 19:18:35 +0200

    Update app constants.

--- app/src/main/java/com/almissbah/revoluttest/utils/Constants.kt
+++ app/src/main/java/com/almissbah/revoluttest/utils/Constants.kt
@@ -2,4 +2,5 @@ package com.almissbah.revoluttest.utils
 
 const val BASE_URL: String = "https://revolut.duckdns.org/"
 const val REFRESH_PERIOD: Long = 1
+const val INITIAL_DELAY: Long = 0
 const val BASE_RATE: Double = 1.0
