1f34876ec5cc7dcb39bf395d2f6faa78f5789776
mohammed almissbah <mohammed.almissbah@hotmail.com>
2019-12-28 18:31:30 +0200

    Update UI.

--- app/src/main/java/com/almissbah/revotest/data/remote/model/Currency.kt
+++ app/src/main/java/com/almissbah/revotest/data/remote/model/Currency.kt
@@ -6,10 +6,9 @@ import kotlin.math.roundToInt
 data class Currency(var name: String, var rate: Double) {
     var value: Double = 0.0
 
-    var valueToString = "0.0"
-        get() {
-            return value.toString()
-        }
+    fun valueToString(): String {
+        return value.toString()
+    }
 
     fun calculateValue(baseValue: Double) {
         value = ((baseValue * rate) * 100).roundToInt() / 100.0
