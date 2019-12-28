526a60f4c5e90683662dfac7b016bb668ef8be2f
mohammed almissbah <mohammed.almissbah@hotmail.com>
2019-12-28 21:42:52 +0200

    Add OkHttpClient and connection timeout to retrofit.

--- app/src/main/java/com/almissbah/revotest/di/module/ApiModule.kt
+++ app/src/main/java/com/almissbah/revotest/di/module/ApiModule.kt
@@ -3,16 +3,19 @@ package com.almissbah.revotest.di.module
 import com.almissbah.revotest.data.RevoRepository
 import com.almissbah.revotest.data.remote.CurrenciesApiService
 import com.almissbah.revotest.utils.BASE_URL
+import com.almissbah.revotest.utils.CONNECTION_TIMEOUT
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
-import dagger.Reusable
 import io.reactivex.schedulers.Schedulers
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
+
 
 @Module
 object ApiModule {
@@ -21,16 +24,28 @@ object ApiModule {
     @Singleton
     @JvmStatic
     internal fun provideGson(): Gson {
-        val gsonBuilder = GsonBuilder()
-        return gsonBuilder.create()
+        return GsonBuilder().create()
     }
 
     @Provides
     @Singleton
     @JvmStatic
-    internal fun provideRetrofitInterface(gson: Gson): Retrofit {
+    internal fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(CONNECTION_TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(CONNECTION_TIMEOUT, TimeUnit.SECONDS)
+            .writeTimeout(CONNECTION_TIMEOUT, TimeUnit.SECONDS)
+            .build()
+    }
+
+
+
+    @Provides
+    @Singleton
+    @JvmStatic
+    internal fun provideRetrofitInterface(gson: Gson, client: OkHttpClient): Retrofit {
         return Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl(BASE_URL).client(client)
             .addConverterFactory(GsonConverterFactory.create(gson))
             .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
             .build()
--- app/src/main/java/com/almissbah/revotest/utils/Constants.kt
+++ app/src/main/java/com/almissbah/revotest/utils/Constants.kt
@@ -1,6 +1,7 @@
 package com.almissbah.revotest.utils
 
 const val BASE_URL: String = "https://revolut.duckdns.org/"
+const val CONNECTION_TIMEOUT: Long = 30
 const val REFRESH_PERIOD: Long = 1
 const val INITIAL_DELAY: Long = 0
 const val BASE_RATE: Double = 1.0
