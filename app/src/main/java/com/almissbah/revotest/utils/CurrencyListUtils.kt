fd2c1a7151739c4c732a9494490e8984844847a6
mohammed almissbah <mohammed.almissbah@hotmail.com>
2019-12-29 13:27:25 +0200

    Add KDocs for CurrencyListUtils.

--- app/src/main/java/com/almissbah/revotest/utils/CurrencyListUtils.kt
+++ app/src/main/java/com/almissbah/revotest/utils/CurrencyListUtils.kt
@@ -5,17 +5,39 @@ import com.almissbah.revotest.data.remote.model.Currency
 import com.google.gson.Gson
 
 class CurrencyListUtils {
+
     companion object {
+
+        /**
+         * Converts user input value to a valid double
+         *
+         * @param text user input as string.
+         * @return 0.0 if string is invalid double or converted double otherwise.
+         */
         fun toValidDouble(text: String): Double {
             var value = 0.0
             if (text != "" && text != ".") value = java.lang.Double.parseDouble(text)
             return value
         }
 
+        /**
+         * Calculates EUR value from current base currency
+         *
+         * @param userInput
+         * @param currency current base currency
+         * @return value in EUR
+         */
         fun calculateBaseValue(userInput: Double, currency: Currency?): Double {
             return userInput / (currency?.rate ?: 1.0)
         }
 
+
+        /**
+         * Build response object from json string
+         *
+         * @param jsonString
+         * @return CurrenciesApiResponse instance
+         */
         fun parseResponseFromJson(jsonString: String): CurrenciesApiResponse? {
             return Gson().fromJson<CurrenciesApiResponse>(
                 jsonString,
@@ -23,6 +45,13 @@ class CurrencyListUtils {
             )
         }
 
+
+        /**
+         * Build currencies list from response object
+         *
+         * @param response CurrenciesApiResponse instance
+         * @return MutableList<Currency> instance
+         */
         fun generateListFromResponse(response: CurrenciesApiResponse): MutableList<Currency> {
             val tempList = mutableListOf<Currency>()
             val dataSet = response.rates.entries
@@ -35,6 +64,16 @@ class CurrencyListUtils {
             return tempList
         }
 
+
+        /**
+         * Search the list for baseCurrency name to update currency the value and returns the first
+         * element from the list if baseCurrency name not found.
+         *
+         * @param baseValue current base value in EUR
+         * @param baseCurrency current base currency
+         * @param currencies updated currency list
+         * @return
+         */
         fun getValidBaseCurrency(
             baseValue: Double,
             baseCurrency: Currency?,
@@ -54,6 +93,14 @@ class CurrencyListUtils {
             return currency
         }
 
+
+        /**
+         * Updates list elements with a new base value in EUR and create a new instances for the same values.
+         *
+         * @param baseValue base value in EUR
+         * @param forList
+         * @return updated list
+         */
         fun calculateCorrespondingValues(
             baseValue: Double,
             forList: MutableList<Currency>
@@ -67,7 +114,13 @@ class CurrencyListUtils {
             return list
         }
 
-
+        /**
+         * Generates a new list by ordering its elements base on the old list order.
+         *
+         * @param fromList list to copy order from
+         * @param toList list to order by fromlist
+         * @return
+         */
         fun copyListItemsOrder(
             fromList: MutableList<Currency>,
             toList: MutableList<Currency>
--- app/src/test/java/com/almissbah/revotest/utils/CurrencyListUtilsTest.kt
+++ app/src/test/java/com/almissbah/revotest/utils/CurrencyListUtilsTest.kt
@@ -5,11 +5,11 @@ import org.junit.Assert.*
 import org.junit.Test
 
 class CurrencyListUtilsTest {
-    val sampleResponce1 =
+    private val sampleResponce1 =
         "{\"base\":\"EUR\",\"date\":\"2018-09-06\",\"rates\":{\"AUD\":1.6164,\"BGN\":1.9558,\"BRL\":4.7919,\"CAD\":1.5338,\"CHF\":1.1275,\"CNY\":7.9452,\"CZK\":25.715,\"DKK\":7.4568,\"GBP\":0.89825,\"HKD\":9.1325,\"HRK\":7.4342,\"HUF\":326.49,\"IDR\":17324.0,\"ILS\":4.1707,\"INR\":83.719,\"ISK\":127.8,\"JPY\":129.55,\"KRW\":1304.8,\"MXN\":22.366,\"MYR\":4.8121,\"NOK\":9.7761,\"NZD\":1.7633,\"PHP\":62.593,\"PLN\":4.3184,\"RON\":4.6386,\"RUB\":79.576,\"SEK\":10.591,\"SGD\":1.6,\"THB\":38.131,\"TRY\":7.6283,\"USD\":1.1634,\"ZAR\":17.824}}"
-    val sampleResponce2 =
+    private val sampleResponce2 =
         "{\"base\":\"EUR\",\"date\":\"2018-09-06\",\"rates\":{\"HKD\":9.1329,\"HRK\":7.4345,\"AUD\":1.6165,\"BGN\":1.9559,\"BRL\":4.7921,\"CAD\":1.5339,\"CHF\":1.1276,\"CNY\":7.9456,\"CZK\":25.716,\"DKK\":7.4571,\"GBP\":0.89829,\"HUF\":326.51,\"IDR\":17325.0,\"ILS\":4.1708,\"INR\":83.722,\"ISK\":127.81,\"JPY\":129.56,\"KRW\":1304.8,\"MXN\":22.367,\"MYR\":4.8123,\"NOK\":9.7766,\"NZD\":1.7634,\"PHP\":62.596,\"PLN\":4.3185,\"RON\":4.6388,\"RUB\":79.579,\"SEK\":10.591,\"SGD\":1.6001,\"THB\":38.132,\"TRY\":7.6286,\"USD\":1.1635,\"ZAR\":17.824}}"
-    val delta = 0.0
+    private val delta = 0.0
 
     @Test
     fun testCovertStringToDouble() {
@@ -20,16 +20,31 @@ class CurrencyListUtilsTest {
 
     @Test
     fun testCalculateBaseValue() {
-        assertEquals(CurrencyListUtils.calculateBaseValue(1.1, Currency("USD", 1.0)), 1.1, delta)
-        assertEquals(CurrencyListUtils.calculateBaseValue(15.0, Currency("USD", 5.0)), 3.0, delta)
-        assertEquals(CurrencyListUtils.calculateBaseValue(1.1, null), 1.1, delta)
+        assertEquals(
+            "Should equal 1.1",
+            CurrencyListUtils.calculateBaseValue(1.1, Currency("USD", 1.0)),
+            1.1,
+            delta
+        )
+        assertEquals(
+            "Should equal 3.0",
+            CurrencyListUtils.calculateBaseValue(15.0, Currency("USD", 5.0)),
+            3.0,
+            delta
+        )
+        assertEquals(
+            "Should equal 1.1",
+            CurrencyListUtils.calculateBaseValue(1.1, null),
+            1.1,
+            delta
+        )
     }
 
     @Test
     fun testParseResponseFromJson() {
         val response = CurrencyListUtils.parseResponseFromJson(sampleResponce1)
-        assertEquals(response!!.base, "EUR")
-        assertEquals(response.rates.size, 32)
+        assertEquals("Base currency should equal EUR", response!!.base, "EUR")
+        assertEquals("Currencies count should equal 32", response.rates.size, 32)
     }
 
 
@@ -37,16 +52,21 @@ class CurrencyListUtilsTest {
     fun testGenerateListFromResponse() {
         val response = CurrencyListUtils.parseResponseFromJson(sampleResponce1)
         val list = CurrencyListUtils.generateListFromResponse(response!!)
-        assertEquals(list.first().name, "EUR")
-        assertEquals(list.size, 33)
+        assertEquals("First currency name should equal EUR", list.first().name, "EUR")
+        assertEquals("Currency list size should equal 33", list.size, 33)
     }
 
     @Test
     fun testGetValidBaseCurrency() {
         val response = CurrencyListUtils.parseResponseFromJson(sampleResponce1)
         val list = CurrencyListUtils.generateListFromResponse(response!!)
-        assertEquals(CurrencyListUtils.getValidBaseCurrency(1.0, null, list).name, "EUR")
         assertEquals(
+            "Currency should equal EUR",
+            CurrencyListUtils.getValidBaseCurrency(1.0, null, list).name,
+            "EUR"
+        )
+        assertEquals(
+            "Currency should equal USD",
             CurrencyListUtils.getValidBaseCurrency(1.0, Currency("USD", 1.1), list).name,
             "USD"
         )
@@ -55,14 +75,14 @@ class CurrencyListUtilsTest {
     @Test
     fun testCalculateCorrespondingValues() {
         val response = CurrencyListUtils.parseResponseFromJson(sampleResponce1)
-        val list = CurrencyListUtils.generateListFromResponse(response!!)
-        val newList = CurrencyListUtils.calculateCorrespondingValues(1.0, list)
+        val oldList = CurrencyListUtils.generateListFromResponse(response!!)
+        val newList = CurrencyListUtils.calculateCorrespondingValues(2.0, oldList)
 
-        list.first().calculateValue(2.0)
-        assertEquals(list.first().value, newList.first().value * 2, delta)
+        oldList.first().calculateValue(2.0)
+        assertEquals(oldList.first().value, newList.first().value, delta)
 
-        list[5].calculateValue(2.0)
-        assertEquals(list[5].value, newList[5].value * 2, delta)
+        oldList[5].calculateValue(2.0)
+        assertEquals(oldList[5].value, newList[5].value, delta)
     }
 
 
